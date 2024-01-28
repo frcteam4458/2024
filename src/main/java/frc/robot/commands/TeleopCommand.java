2c142e65c80bfc362680e3c86117e52719f6bb15
Keebler17 <keebler17.exe@gmail.com>
2024-01-28 12:51:05 -0500

    remove extraneous printcommand

--- src/main/java/frc/robot/commands/TeleopCommand.java
+++ src/main/java/frc/robot/commands/TeleopCommand.java
@@ -4,15 +4,12 @@
 
 package frc.robot.commands;
 
-import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.drive.DriveSubsystem;
@@ -57,7 +54,6 @@ public class TeleopCommand extends Command {
 
   @Override
   public void execute() {    
-    new PrintCommand(driveChooser.getSelected() + "").schedule();
     if(driveChooser.getSelected() == 1)
       driveSubsystem.arcadeDriveFieldOriented(getX(), getY(), getOmega());
     else
